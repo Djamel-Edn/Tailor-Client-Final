ad69ecd5b198f5c6824db61ae29790ee106b82e2
Djamel <d.dib@esi-sba.dz>
2024-04-24 21:51:35 +0100

    chat v2.0.3

--- backend/api/chatController.js
+++ backend/api/chatController.js
@@ -1,5 +1,5 @@
 const chatModel = require('../Models/chatModel');
-const clientmodel = require('../Models/clientmodel');
+const clientModel = require('../Models/clientmodel');
 const tailorModel = require('../Models/tailorModel');
 
 const createOrFindChat = async (req, res) => {
@@ -7,13 +7,12 @@ const createOrFindChat = async (req, res) => {
 
     try {
         // Check if both client and tailor IDs are provided
-        console.log("test")
         if (!clientId || !tailorId) {
             return res.status(400).json({ error: 'Both client and tailor IDs are required' });
         }
 
         // Assuming clientModel and tailorModel are imported from their respective files
-        const client = await clientmodel.findById(clientId);
+        const client = await clientModel.findById(clientId);
         const tailor = await tailorModel.findById(tailorId);
 
         if (!client) {
@@ -24,33 +23,28 @@ const createOrFindChat = async (req, res) => {
             return res.status(404).json({ error: 'Tailor not found' });
         }
 
-        // Find the chat between the client and tailor and populate client, tailor, and messages
-        let chat = await chatModel.findOne({ client: clientId, tailor: tailorId })
-                                   .populate({
-                                       path: 'client',
-                                       model: 'Client'
-                                   })
-                                   .populate({
-                                       path: 'tailor',
-                                       model: 'Tailor'
-                                   })
-                                   .populate({
-                                       path: 'messages',
-                                       model: 'Message'
-                                   });
+        // Create a new chat
+        const newChat = new chatModel({ client: clientId, tailor: tailorId });
+        delete client.password;
+        delete tailor.password
+        // Populate client and tailor fields
+        newChat.client = client;
+        newChat.tailor = tailor;
 
-        // If chat doesn't exist, create a new one
-        if (!chat) {
-            const newChat = new chatModel({ client: clientId, tailor: tailorId });
-            chat = await newChat.save();
-        }
+        // Optionally, you can populate the messages field if needed
+        // For example:
+        // const messages = await Message.find({ chat: newChat._id });
+        // newChat.messages = messages;
 
-        res.status(200).json(chat);
+        const savedChat = await newChat.save();
+
+        res.status(200).json(savedChat);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Server error' });
     }
 };
+
 
 
 
