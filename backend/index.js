0107bfc2276d73ed40a334f3ddd81b62a955c675
Dib Djamel Eddine <131314220+Djamel-Edn@users.noreply.github.com>
2024-04-29 23:38:15 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const { Server } = require("socket.io");
 const http = require("http"); 
 
 app.use(cors({
-    origin: 'http://localhost:5000',
+    origin: 'http://localhost:5000/login',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Origin', 'Content-Type', 'Accept', 'Authorization', 'X-Requested-With'],
     credentials: true
