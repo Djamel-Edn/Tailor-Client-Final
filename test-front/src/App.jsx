99172b97b67bd400242b966703740198cb60ebdc
Djamel <d.dib@esi-sba.dz>
2024-05-02 15:31:18 +0100

    LandungPage

--- test-front/src/App.jsx
+++ test-front/src/App.jsx
@@ -1,12 +1,13 @@
 import {Routes, Route, Navigate} from 'react-router-dom'
-import Login from './../pages/login';
-import RegisterClient from './../pages/registerClient';
-import RegisterTailor from './../pages/registerTailor';
-import Forgotpassword from './../pages/forgotpassword';
-import ResetPassword from './../pages/resetPassword';
-import Profile from '../pages/profile';
-import Home from '../pages/home';
-import Recherche from '../pages/recherche';
+import Login from './pages/login';
+import RegisterClient from './pages/registerClient';
+import RegisterTailor from './pages/registerTailor';
+import Forgotpassword from './pages/forgotpassword';
+import Profile from './pages/profile';
+import Home from './pages/home';
+import Recherche from './pages/recherche';
+import LandingPage from './pages/LandingPage';
+
 function App() {
 
 
@@ -18,12 +19,11 @@ function App() {
         <Route path="/registerClient" element={<RegisterClient />} />
         <Route path="/registerTailor" element={<RegisterTailor />} />
         <Route path="/login/forgotpassword" element={<Forgotpassword />} />
-        <Route path="/:email/:resettoken" element={<ResetPassword />} />
-        <Route path="/" element={<Forgotpassword />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/profile" element={<Profile/>} />
         <Route path="/home" element={<Home/>} />
         <Route path="/recherche" element={<Recherche/>} />
-       
+        <Route path="/*" element={<Recherche/>} />
         <Route path='*' element={<Navigate  to='/'/>}/>
       </Routes>
     
